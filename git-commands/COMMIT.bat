9bba427cc4dac5feed4d92aa15b1009611859ba5
isaaclins <isaac.lins07@gmail.com>
2023-06-07 14:38:26 +0200

    added separate file structure and also updated with GUI, delete, save to file AND add new

---
+++ git-commands/COMMIT.bat
@@ -0,0 +1,13 @@
+@echo off
+setlocal
+
+echo started file
+
+git add .
+echo added files
+
+set /p message="Enter your commit message: "
+echo added message
+
+git commit -m "%message%"
+echo committed changes
